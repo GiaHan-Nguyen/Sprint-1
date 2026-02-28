116d374af1f8fd8f0989cb47327a7bca3840f793
GiaHan-Nguyen <giahan.nguyen@mnsu.edu>
2026-02-28 15:38:59 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    public class Calculator
+    {
+        // Private fields (Encapsulation)
+        private double num1;
+        private double num2;
+
+        // Public methods
+        public double Add(double a, double b)
+        {
+            num1 = a;
+            num2 = b;
+            return num1 + num2;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            num1 = a;
+            num2 = b;
+            return num1 - num2;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            num1 = a;
+            num2 = b;
+            return num1 * num2;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+
+            num1 = a;
+            num2 = b;
+            return num1 / num2;
+        }
+    }
+}
